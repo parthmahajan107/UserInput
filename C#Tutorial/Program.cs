a520baa300c7b61a021b00acbc02ab07606d68f0
parth <parth@PARTH>
2023-03-09 10:39:56 +0530

    Add project files.

---
+++ C#Tutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write first number: ");
+            int a = Convert.ToInt32(Console.ReadKey());
+
+            Console.WriteLine("Write second number: ");
+            int b = Convert.ToInt32(Console.ReadKey());
+
+            int c = a + b; 
+
+            Console.WriteLine("Sum of both the numbers: "+ c);
+            Console.ReadKey();
+        }
+    }
+}
